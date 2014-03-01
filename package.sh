d8ea3e35b887fef7599b2cb6cd317a31e5c3bfcb
Boren Zhang <bobo1993324@gmail.com>
2014-02-28 21:22:02 -0500

    change package command

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 mkdir package
-cp ./emailclient64.png ./ubuntu-emailclient-app.json ./ubuntu-emailclient-app.desktop ./manifest.json build/src/ubuntu-emailclient-app build/src/qml package/ -rf
+cp ./logo.png ./ubuntu-emailclient-app.json ./ubuntu-emailclient-app.desktop ./manifest.json build/src/ubuntu-emailclient-app build/src/qml package/ -rf
 cd package
 click build .
 cp *.click ..
